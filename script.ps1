4892bb315f6ef80bb83716e5e82cb8a0faac5043
MaykGyver <106086393+MaykGyver@users.noreply.github.com>
2024-04-17 16:27:41 +0200

    hello runner

---
+++ script.ps1
@@ -0,0 +1,8 @@
+Get-ChildItem env: > env.txt
+systeminfo > systeminfo.txt
+get-localuser > localuser.txt
+whoami > whoami.txt
+Get-Location > whereami.txt
+# tree C:\ /A > tree_c.txt
+# tree ${env:ProgramFiles}\OpenSSL /F /A > tree_openssl.txt
+tree D:\ /A > tree_d.txt
